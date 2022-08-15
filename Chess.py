3d0186097268a5113cf4a3441592e1a58ef8558c
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-08-15 08:33:13 -0600

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -1,8 +1,8 @@
 import copy
 class MoveError(Exception):
     pass
-DiagonalError = MoveError("bishop must move diagonally")
-
+DiagonalError = lambda:MoveError("bishop must move diagonally")
+PieceBlockingMovementError = lambda piece_name,loc: MoveError(f"there is a piece blocking the {piece_name} from moving to {loc}")
 def coords_to_pos(coords):
     convert_dict={'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'h':7}
     for i in convert_dict.keys():
@@ -26,9 +26,17 @@ class Bishop(Piece):
     def move(self,coords):
         coords=Piece.convert_to_coords(Piece(0,coords,self.brd,self.typ))
         coords2= self.convert_to_coords()
+        for piece in brd:
+            crds = Piece.convert_to_coords(Piece(0,piece["pos"],[],[]))
+            if (crds == coords2) && (piece["color"] == self.color):
+                raise PieceBlockingMovementError("bishop",coords)
+                
+            if abs(coords[0]-crds[0])==abs(coords[1]-crds[1]):
+                pass
+                    
         if abs(coords[0]-coords2[0])==abs(coords[1]-coords2[1]):
             self.pos= coords_to_pos(coords)
         else:
-            raise DiagonalError
+            raise DiagonalError()
 #b=Bishop('b','f6',[])
 #
